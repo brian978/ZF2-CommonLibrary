bdb3969f66adbfc87a0c7c63a84d2b80e3635964
brian978 <andrei.dincescu@hotmail.com>
2013-07-19 08:11:44 +0300

    Database model factory in progress...

---
+++ module/Library/src/Library/Model/DbModelFactory.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * ZF2-CommonLibrary
+ *
+ * @link      https://github.com/brian978/ZF2-CommonLibrary
+ * @copyright Copyright (c) 2013
+ * @license   Creative Commons Attribution-ShareAlike 3.0
+ */
+
+namespace Library\Model;
+
+use Zend\ServiceManager\AbstractFactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class DbModelFactory implements AbstractFactoryInterface
+{
+    /**
+     * Determine if we can create a service with name
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @param                         $name
+     * @param                         $requestedName
+     * @return bool
+     */
+    public function canCreateServiceWithName(ServiceLocatorInterface $serviceLocator, $name, $requestedName)
+    {
+        // TODO: Implement canCreateServiceWithName() method.
+    }
+
+    /**
+     * Create service with name
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @param                         $name
+     * @param                         $requestedName
+     * @return mixed
+     */
+    public function createServiceWithName(ServiceLocatorInterface $serviceLocator, $name, $requestedName)
+    {
+        // TODO: Implement createServiceWithName() method.
+    }
+}
