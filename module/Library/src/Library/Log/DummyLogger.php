f2f7f3e8a9f87c445ecd801199a277d72b37fc72
brian978 <andrei.dincescu@hotmail.com>
2013-07-28 22:41:19 +0300

    Added logger object into the database models

---
+++ module/Library/src/Library/Log/DummyLogger.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * ZF2-CommonLibrary
+ *
+ * @link      https://github.com/brian978/ZF2-CommonLibrary
+ * @copyright Copyright (c) 2013
+ * @license   Creative Commons Attribution-ShareAlike 3.0
+ */
+
+namespace Library\Log;
+
+use Zend\Log\Logger as ZendLogger;
+
+/**
+ * Class Logger
+ *
+ * This is just a dummy logger that the models will use by default
+ *
+ * @package Library\Log
+ */
+class DummyLogger extends ZendLogger
+{
+    /**
+     * Dummy version of the log method
+     *
+     * @param int   $priority
+     * @param mixed $message
+     * @param array $extra
+     * @return $this|ZendLogger
+     */
+    public function log($priority, $message, $extra = array())
+    {
+        return $this;
+    }
+}
---
+++ module/Library/tests/TestHelpers/Model/DbModel.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * ZF2-CommonLibrary
+ *
+ * @link      https://github.com/brian978/ZF2-CommonLibrary
+ * @copyright Copyright (c) 2013
+ * @license   Creative Commons Attribution-ShareAlike 3.0
+ */
+
+namespace TestHelpers\Model;
+
+use Library\Model\AbstractDbModel;
+
+class DbModel extends AbstractDbModel
+{
+    /**
+     * @param $object
+     * @return mixed
+     */
+    protected function doInsert($object)
+    {
+        // TODO: Implement doInsert() method.
+    }
+
+    /**
+     * @param $object
+     * @return mixed
+     */
+    protected function doUpdate($object)
+    {
+        // TODO: Implement doUpdate() method.
+    }
+
+    /**
+     * @param $object
+     * @return mixed
+     */
+    public function doDelete($object)
+    {
+        // TODO: Implement doDelete() method.
+    }
+}
