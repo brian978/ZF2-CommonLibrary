2bb5c5a08328b9f2c2fb83460f1d8ceb3fcc002b
Andrei Dincescu <andrei.dincescu@hotmail.com>
2013-08-30 09:26:12 +0300

    Updated comments

--- module/Library/src/Library/Model/Mapper/AbstractMapper.php
+++ module/Library/src/Library/Model/Mapper/AbstractMapper.php
@@ -2,7 +2,9 @@
 /**
  * ZF2-CommonLibrary
  *
- * @author Andrei Dincescu <andrei.dincescu@ubisoft.com
+ * @link      https://github.com/brian978/ZF2-CommonLibrary
+ * @copyright Copyright (c) 2013
+ * @license   Creative Commons Attribution-ShareAlike 3.0
  */
 
 namespace Library\Model\Mapper;
